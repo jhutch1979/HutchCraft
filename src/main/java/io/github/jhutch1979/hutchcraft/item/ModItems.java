394522f371c3d0e168f85270edc86ac0ac9b337b
John Hutchinson <john@upnorth.net>
2022-08-08 12:33:19 -0400

    /added pulsating iron ingot and nuget

---
+++ src/main/java/io/github/jhutch1979/hutchcraft/item/ModItems.java
@@ -0,0 +1,24 @@
+package io.github.jhutch1979.hutchcraft.item;
+
+import io.github.jhutch1979.hutchcraft.HutchCraft;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HutchCraft.MOD_ID);
+
+    public static final RegistryObject<Item> PULSATING_IRON_INGOT = ITEMS.register("pulsating_iron_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> PULSATING_IRON_NUGET = ITEMS.register("pulsating_iron_nuget",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
